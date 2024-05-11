4308e6004432edb3fa271f2d8b322a0bb06dce4d
CAVALCANTE NOA p2107540 <noa.cavalcante@etu.univ-lyon1.fr>
2024-05-11 19:38:23 +0200

    Escape some chars

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -5,10 +5,11 @@ interface LandingComponentProps {
 export default function LandingComponent({className = ""}: Readonly<LandingComponentProps>) {
   return (
     <div className={"h-landing -mb-10 flex flex-col justify-center gap-10 " + className}>
-      <h1><span className={"text-blue-500"}>Hello, I'm Noa,</span><br/> Web & mobile developer</h1>
+      <h1><span className={"text-blue-500"}>Hello, I&apos;m Noa,</span><br/> Web & mobile developer</h1>
       <p className={"text-xs"}>
-        I'm passionate about technology and I love to create web and mobile applications. I'm always looking for new
-        challenges and I'm constantly learning new technologies.
+        I&apos;m passionate about technology and I love to create web and mobile applications. I&apos;m always looking
+        for new
+        challenges and I&apos;m constantly learning new technologies.
       </p>
     </div>
   )
